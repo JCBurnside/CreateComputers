b97e933d60160078ad34285afe1ae8f2efa9eafb
digital <jcburnside97@gmail.com>
2023-02-02 21:03:10 -0500

    test of CC successful.  now moving to create

--- src/main/kotlin/pixellabs/minecraftmods/tileentities/TestingAPITileEntity.kt
+++ src/main/kotlin/pixellabs/minecraftmods/tileentities/TestingAPITileEntity.kt
@@ -7,8 +7,11 @@ import net.minecraft.world.level.block.state.BlockState
 import pixellabs.minecraftmods.Registration
 import pixellabs.minecraftmods.TestingPeripheral
 import dan200.computercraft.shared.Capabilities.CAPABILITY_PERIPHERAL
+import net.minecraft.core.Direction
 import net.minecraftforge.common.capabilities.Capability
 import net.minecraftforge.common.util.LazyOptional
+import org.apache.logging.log4j.Level
+import pixellabs.minecraftmods.CreateComputers
 
 
 class TestingAPITileEntity(private val pos : BlockPos,private var state : BlockState)
@@ -17,14 +20,14 @@ class TestingAPITileEntity(private val pos : BlockPos,private var state : BlockS
 
     private val peripheral = TestingPeripheral(this)
     private var peripheralCap : LazyOptional<IPeripheral>? = null
-    override fun <T : Any?> getCapability(cap: Capability<T>): LazyOptional<T> {
+    override fun <T : Any?> getCapability(cap: Capability<T>, side: Direction?): LazyOptional<T> {
         return if(cap == CAPABILITY_PERIPHERAL) {
             if(peripheralCap == null) {
                 peripheralCap = LazyOptional.of { peripheral }
             }
             peripheralCap!!.cast()
         } else {
-            super.getCapability(cap)
+            super.getCapability(cap,side)
         }
     }
 }
